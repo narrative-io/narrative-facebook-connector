eb863e020ae6b20468d273405f86c69111a69549
Marko Babic <marko@narrative.io>
2022-06-29 20:15:25 -0700

    backend: add auth, logging middleware

---
+++ backend/api/src/main/scala/io/narrative/connectors/facebook/routes/Logging.scala
@@ -0,0 +1,43 @@
+package io.narrative.connectors.facebook.routes
+
+import cats.effect._
+import cats.syntax.option._
+import com.typesafe.scalalogging.LazyLogging
+import org.http4s.{HttpApp, Response}
+import org.http4s.server.middleware._
+
+object Logging extends LazyLogging {
+  def apply(routes: HttpApp[IO])(implicit cs: ContextShift[IO]): HttpApp[IO] = {
+    // The order that these middleware are stacked matters. Any other combination and the X-Request-ID header won't
+    // propagate to the response when the server throws an exception.
+    errorResponseLogging {
+      RequestId.httpApp {
+        RequestLogger.httpAppLogBodyText(logHeaders = true, logBody = truncateBody(8192)) {
+          ErrorHandling(routes)
+        }
+      }
+    }
+  }
+
+  def errorResponseLogging(service: HttpApp[IO]): HttpApp[IO] = {
+    val logInfo = (s: String) => IO(logger.info(s))
+    val logWarn = (s: String) => IO(logger.warn(s))
+    HttpApp[IO] { req =>
+      for {
+        resp <- service(req)
+        _ <- resp.status.code match {
+          case code if code >= 400 =>
+            org.http4s.internal.Logger.logMessageWithBodyText[IO, Response[IO]](resp)(
+              logHeaders = true,
+              logBodyText = truncateBody(32768) _
+            )(logWarn)
+          case _ =>
+            Logger.logMessage[IO, Response[IO]](resp)(logHeaders = true, logBody = false)(logInfo)
+        }
+      } yield resp
+    }
+  }
+
+  private def truncateBody(maxBytes: Int)(bytes: fs2.Stream[IO, Byte]): Option[IO[String]] =
+    bytes.take(maxBytes.toLong).through(fs2.text.utf8Decode).compile.last.map(_.getOrElse("")).some
+}
