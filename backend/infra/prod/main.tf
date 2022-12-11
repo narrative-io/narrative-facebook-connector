6989c7326f9b9aba93595c5d3dd1afb806179e26
Sami Dalouche <sami@narrative.io>
2022-12-12 08:37:20 +1300

    terraform: do not hardcode AWS object ids

--- backend/infra/prod/main.tf
+++ backend/infra/prod/main.tf
@@ -1,137 +1,145 @@
 locals {
-  app_port = 8080
-  api_image_tag = "0.0.15"
-  api_image_repository = "narrative-facebook-connector/api"
-  domain_name = "facebook.narrativeconnectors.com"
-  name_prefix = "facebook-connector"
-  ssm_db_username = "/${local.stage}/connectors/facebook/api/facebookconnector-db/user"
-  ssm_db_password = "/${local.stage}/connectors/facebook/api/facebookconnector-db/password"
+  app_port                 = 8080
+  api_image_tag            = "0.0.15"
+  api_image_repository     = "narrative-facebook-connector/api"
+  domain_name              = "facebook.narrativeconnectors.com"
+  name_prefix              = "facebook-connector"
+  ssm_db_username          = "/${local.stage}/connectors/facebook/api/facebookconnector-db/user"
+  ssm_db_password          = "/${local.stage}/connectors/facebook/api/facebookconnector-db/password"
   ssm_narrative_api_client = "/${local.stage}/connectors/facebook/openapi/client"
   ssm_narrative_api_secret = "/${local.stage}/connectors/facebook/openapi/secret"
-  worker_image_tag = "0.0.15"
-  worker_image_repository = "narrative-facebook-connector/worker"
-  stage = "prod"
-  vpc_id      = "vpc-081651050f8c197a5"
+  worker_image_tag         = "0.0.15"
+  worker_image_repository  = "narrative-facebook-connector/worker"
+  stage                    = "prod"
+}
 
-  private_subnet_ids = [
-    "subnet-0cb2ab40f0b64f414",
-    "subnet-08f5db56f4e2fb0b6",
-    "subnet-0ee858647f53ab935",
-    "subnet-034d755cf159c1210",
-    "subnet-0d23436682672ee7b",
-    "subnet-022e2cec94ba78b83",
-    ]
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["main-${local.stage}"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
+  tags = {
+    Tier = "private"
+  }
 }
 
 module "alerts-api" {
-  source = "../modules/alerts"
-  stage = local.stage
-  filter_name = "alerts"
+  source         = "../modules/alerts"
+  stage          = local.stage
+  filter_name    = "alerts"
   log_group_name = module.fargate_api.log_group_name
-  log_group_arn = module.fargate_api.log_group_arn
+  log_group_arn  = module.fargate_api.log_group_arn
 }
 
 module "alerts-worker" {
-  source = "../modules/alerts"
-  stage = local.stage
-  filter_name = "alerts"
+  source         = "../modules/alerts"
+  stage          = local.stage
+  filter_name    = "alerts"
   log_group_name = module.fargate_worker.log_group_name
-  log_group_arn = module.fargate_worker.log_group_arn
+  log_group_arn  = module.fargate_worker.log_group_arn
 }
 
 data "aws_ssm_parameter" "datadog_api_key" {
-  name = "/shared/datadog/api-key"
+  name            = "/shared/datadog/api-key"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "datadog_app_key" {
-  name = "/shared/datadog/app-key"
+  name            = "/shared/datadog/app-key"
   with_decryption = true
 }
 
 module "api_gateway" {
-  source = "../modules/api"
+  source        = "../modules/api"
   api_image_tag = local.api_image_tag
-  api_version = "v1"
-  app_port = local.app_port
-  aws_lb_arn = module.load_balancer.aws_lb_arn
-  is_public = true
-  name_prefix = local.name_prefix
-  stage = local.stage
-  vpc_id = local.vpc_id
+  api_version   = "v1"
+  app_port      = local.app_port
+  aws_lb_arn    = module.load_balancer.aws_lb_arn
+  is_public     = true
+  name_prefix   = local.name_prefix
+  stage         = local.stage
+  vpc_id        = data.aws_vpc.main.id
 }
 
 module "custom_domain" {
-  source = "../modules/custom_domain"
-  domain_name = local.domain_name
-  api_gateway_id = module.api_gateway.aws_api_gateway_id
+  source                 = "../modules/custom_domain"
+  domain_name            = local.domain_name
+  api_gateway_id         = module.api_gateway.aws_api_gateway_id
   api_gateway_stage_name = module.api_gateway.aws_api_gateway_stage_name
 }
 
 module "fargate_api" {
-  source = "../modules/fargate_api"
-  app_port = local.app_port
+  source                  = "../modules/fargate_api"
+  app_port                = local.app_port
   aws_lb_target_group_arn = module.load_balancer.aws_lb_target_group_arn
-  aws_iam_role_arn = module.iam.aws_iam_role_arn
-  cpu = "1024"
-  desired_count = 1
-  ephemeral_storage = 128
-  image_tag = local.api_image_tag
-  memory = "8192"
-  name_prefix = "${local.name_prefix}-api"
-  private_subnet_ids = local.private_subnet_ids
-  repository_name = local.api_image_repository
-  security_group_id = module.security_group.aws_security_group_id
-  ssm_db_username = local.ssm_db_username
-  ssm_db_password = local.ssm_db_password
-  stage = local.stage
-  token_kms_key_id = module.iam.token_kms_key_id
-  vpc_id = local.vpc_id
+  aws_iam_role_arn        = module.iam.aws_iam_role_arn
+  cpu                     = "1024"
+  desired_count           = 1
+  ephemeral_storage       = 128
+  image_tag               = local.api_image_tag
+  memory                  = "8192"
+  name_prefix             = "${local.name_prefix}-api"
+  private_subnet_ids      = data.aws_subnets.private.ids
+  repository_name         = local.api_image_repository
+  security_group_id       = module.security_group.aws_security_group_id
+  ssm_db_username         = local.ssm_db_username
+  ssm_db_password         = local.ssm_db_password
+  stage                   = local.stage
+  token_kms_key_id        = module.iam.token_kms_key_id
+  vpc_id                  = data.aws_vpc.main.id
 }
 
 module "fargate_worker" {
-  source = "../modules/fargate_worker"
-  aws_iam_role_arn = module.iam.aws_iam_role_arn
-  cpu = "1024"
-  desired_count = 1
-  ephemeral_storage = 128
-  image_tag = local.worker_image_tag
-  memory = "8192"
-  name_prefix = "${local.name_prefix}-worker"
-  private_subnet_ids = local.private_subnet_ids
-  repository_name = local.worker_image_repository
-  security_group_id = module.security_group.aws_security_group_id
-  ssm_db_username = local.ssm_db_username
-  ssm_db_password = local.ssm_db_password
+  source                   = "../modules/fargate_worker"
+  aws_iam_role_arn         = module.iam.aws_iam_role_arn
+  cpu                      = "1024"
+  desired_count            = 1
+  ephemeral_storage        = 128
+  image_tag                = local.worker_image_tag
+  memory                   = "8192"
+  name_prefix              = "${local.name_prefix}-worker"
+  private_subnet_ids       = data.aws_subnets.private.ids
+  repository_name          = local.worker_image_repository
+  security_group_id        = module.security_group.aws_security_group_id
+  ssm_db_username          = local.ssm_db_username
+  ssm_db_password          = local.ssm_db_password
   ssm_narrative_api_client = local.ssm_narrative_api_client
   ssm_narrative_api_secret = local.ssm_narrative_api_secret
-  stage = local.stage
-  token_kms_key_id = module.iam.token_kms_key_id
-  vpc_id = local.vpc_id
+  stage                    = local.stage
+  token_kms_key_id         = module.iam.token_kms_key_id
+  vpc_id                   = data.aws_vpc.main.id
 }
 
 module "iam" {
-  source = "../modules/iam"
-  name_prefix = local.name_prefix
-  ssm_key_alias = "alias/ssm-params-${local.stage}"
-  ssm_db_username = local.ssm_db_username
-  ssm_db_password = local.ssm_db_password
+  source                   = "../modules/iam"
+  name_prefix              = local.name_prefix
+  ssm_key_alias            = "alias/ssm-params-${local.stage}"
+  ssm_db_username          = local.ssm_db_username
+  ssm_db_password          = local.ssm_db_password
   ssm_narrative_api_client = local.ssm_narrative_api_client
   ssm_narrative_api_secret = local.ssm_narrative_api_secret
-  stage = local.stage
+  stage                    = local.stage
 }
 
 module "load_balancer" {
-  source = "../modules/lb"
-  app_port = local.app_port
-  name_prefix = local.name_prefix
-  private_subnet_ids = local.private_subnet_ids
-  stage = local.stage
-  vpc_id = local.vpc_id
+  source             = "../modules/lb"
+  app_port           = local.app_port
+  name_prefix        = local.name_prefix
+  private_subnet_ids = data.aws_subnets.private.ids
+  stage              = local.stage
+  vpc_id             = data.aws_vpc.main.id
 }
 
 module "security_group" {
   source = "../modules/security_group"
-  name = "${local.name_prefix}-${local.stage}"
-  vpc_id = local.vpc_id
+  name   = "${local.name_prefix}-${local.stage}"
+  vpc_id = data.aws_vpc.main.id
 }
