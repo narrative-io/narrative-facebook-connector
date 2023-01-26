63dd44400d8af86d7a30203ce16dfcc23754c8c3
Sami Dalouche <sami@narrative.io>
2023-01-26 13:01:38 +1300

    Restrict ingress port

--- backend/infra/modules/security_group/main.tf
+++ backend/infra/modules/security_group/main.tf
@@ -6,21 +6,25 @@ variable "vpc_id" {
   type = string
 }
 
+variable "app_port" {
+  type = number
+}
+
 resource "aws_security_group" "_" {
-  name = var.name
+  name   = var.name
   vpc_id = var.vpc_id
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
